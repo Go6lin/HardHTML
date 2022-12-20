597464bb91f53711feb2c549269b7064f6735a35
Go6lin <zhidkivmi@yandex.ru>
2022-12-20 21:04:41 +0300

    Актуальная версия
    Продвинулся в логике скрипта

--- scripts.js
+++ scripts.js
@@ -1,13 +1,32 @@
 function  highlit () {
     document.body.addEventListener('click', function (event) {
+        let classCharFirst = 'character__first';
+        let classCharSecond = 'character__second';
+        let classCharThird = 'character__third';
+        let classCharForth = 'character__forth';
+        let classElemToChange = '';
         let targ = event.target;
-        console.log(targ.className)
-
-            targ.style.backgroundColor = 'white';
-            targ.style.backgroundImage = 'url("images/stroke.svg")';
-            targ.style.backgroundSize = 'cover';
-            targ.style.backgroundPosition = 'bottom';
-
-
+        if (targ.className.indexOf(classCharFirst)) {
+            classElemToChange = classCharFirst;
+            console.log(classElemToChange);
+        }
+        if (targ.className.indexOf(classCharSecond)) {
+            classElemToChange = classCharSecond;
+            console.log(classElemToChange);
+        }
+        if (targ.className.indexOf(classCharThird)) {
+            classElemToChange = classCharThird;
+            console.log(classElemToChange);
+        }
+        if (targ.className.indexOf(classCharForth)) {
+            classElemToChange = classCharForth;
+            console.log(classElemToChange);
+        }
+        let elemToChange = document.body.querySelector('.' + classElemToChange)
+                elemToChange.style.backgroundColor = 'white';
+                elemToChange.style.backgroundImage = 'url("images/stroke.svg")';
+                elemToChange.style.backgroundSize = 'cover';
+                elemToChange.style.backgroundPosition = 'bottom';
     });
 }
+//&& попробуй через логическое и
